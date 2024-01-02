920a6ffff3e47db26c4da691a976798c9689d83f
amsyary <amsyary.skenza313@gmail.com>
2024-01-02 11:36:20 +0800

    update intl

---
+++ lib/flutter_chat_ui_types.dart
@@ -0,0 +1,13 @@
+library flutter_chat_ui;
+
+//export types
+// export 'src/types/messages/custom_message.dart';
+// export 'src/types/messages/partial_custom.dart';
+// export 'src/types/messages/partial_file.dart';
+// export 'src/types/messages/partial_image.dart';
+// export 'src/types/messages/partial_text.dart';
+// export 'src/types/messages/text_message.dart';
+// export 'src/types/messages/unsupported_message.dart';
+// export 'src/types/preview_data.dart';
+// export 'src/types/room.dart';
+export 'src/types/types_export.dart';
